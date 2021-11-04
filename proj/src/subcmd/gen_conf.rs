d8c7dfc70ce03655da6ddd29c63af7d20fcc2727
dwb <dwb@dwb.ren>
2021-11-04 20:09:12 +0800

    modify(ab):重命名配置名称

--- proj/src/subcmd/gen_conf.rs
+++ proj/src/subcmd/gen_conf.rs
@@ -5,38 +5,40 @@ use std::io::Write;
 
 struct GenConf {
     conf_type: String,
-    out_file: String
+    out_file: String,
 }
 
 
 const BUILDERS_TEMP: &str = include_str!("../static/env");
-const ABMAP_TEMP : &str = include_str!("../static/ab_map");
+const ABMAP_TEMP: &str = include_str!("../static/build-ab");
 
-impl BaseCmd for GenConf{
-    fn run(&self){
-        let v= String::from("build-player");
-        let v2 = String::from("build-ab");
-        match &self.conf_type{
-            v =>{
+impl BaseCmd for GenConf {
+    fn run(&self) {
+        let tar = self.conf_type.as_str();
+        match tar {
+            "build-player" | "env" => {
                 let mut f = std::fs::File::create(&self.out_file).expect(format!("create config file {} failed!", &self.out_file).as_str());
                 f.write_all(BUILDERS_TEMP.as_bytes()).expect(format!("write content to {} failed!", self.out_file).as_str());
                 info!("Gen suc!");
-                }
-            v2 => {
-                const ab_f : &str= "ab_map.yaml";
+            }
+            "build-ab" => {
+                const ab_f: &str = "build_ab.yaml";
                 let mut f = std::fs::File::create(ab_f).expect(format!("create config file {} failed!", ab_f).as_str());
                 f.write_all(ABMAP_TEMP.as_bytes()).expect(format!("write content to {} failed!", ab_f).as_str());
                 info!("Gen suc!");
+            }
+            _ => {
+                error!("{}", format!("Not support conf type {} yet! Do nothing", tar));
             }
         }
     }
 }
 
-impl GenConf{
+impl GenConf {
     fn new(ctype: String, outf: String) -> Self {
-        GenConf{
+        GenConf {
             conf_type: ctype.to_string(),
-            out_file: outf.to_string()
+            out_file: outf.to_string(),
         }
     }
 }
@@ -48,10 +50,6 @@ pub fn handle(subm: &ArgMatches) {
     match target {
         None => {}
         Some(v) => {
-            if !conf_support.contains(&v) {
-                error!("{}", format!("Not support conf type {} yet! Do nothing", v));
-                return;
-            }
             let cmd = &GenConf::new(String::from(v), String::from(o));
             cmd.run();
         }
