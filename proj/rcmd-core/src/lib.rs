9ebc57f1a8cdc4866877251496ffc4e6af2ab38e
dwb <dwb@dwb.ren>
2021-12-10 18:25:21 +0800

    add：zipo

--- proj/rcmd-core/src/lib.rs
+++ proj/rcmd-core/src/lib.rs
@@ -6,6 +6,7 @@ pub mod pkg;
 
 ///透传暴露的lib
 pub mod Ex {
+    pub use walkdir;
     pub use chrono;
 }
 ///日志模块
@@ -29,15 +30,37 @@ pub mod Log {
         Ok(())
     }
 }
-
+///命令行模块
 pub use clap;
 pub use clap::{ArgMatches};
 
+
 mod tests {
+    use crate::util::filesys::zip_dir;
+    use std::path::Path;
+    use walkdir::{WalkDir, DirEntry};
+
+
     #[test]
     fn it_works() {
-        let resp = Get("http://www.baidu.com");
-        println!("test here111");
+         use walkdir;
+        let src_dir = "/Users/mac/data0/public_work/ucmd/proj/rcmd-core/test/ziptest";
+        let pf = Path::new(src_dir);
+        let zipfile = std::fs::File::create("test.zip").unwrap();
+        let mut ws = WalkDir::new(pf);
+        zip_dir(
+            &mut ws.into_iter()
+            .filter_entry(|e|ignore_entry(e))
+            .filter_map(|v|v.ok()), pf.to_str().unwrap(), zipfile);
+    }
+
+    fn ignore_entry(entry: &DirEntry) -> bool {
+        entry
+            .file_name()
+            .to_str()
+            //忽略.开头的文件
+            .map(|s|!s.starts_with("."))
+            .unwrap_or(false)
     }
 }
 
--- proj/rcmd-core/src/util/filesys.rs
+++ proj/rcmd-core/src/util/filesys.rs
@@ -1,6 +1,17 @@
 use std::{io, fs};
-use std::path::Path;
+use std::path::{Path, PathBuf};
+use std::fs::{File};
+use std::io::{Seek, Write, Read};
+use zip::write::FileOptions;
+use walkdir::DirEntry;
 
+
+///拷贝当前文件夹内所有文件到指定文件夹
+///##Exmample
+/// ```
+/// //todo
+/// //copy_dir_all()
+/// ```
 pub fn copy_dir_all(src: impl AsRef<Path>, dst: impl AsRef<Path>) -> io::Result<()> {
     fs::create_dir_all(&dst)?;
     for entry in fs::read_dir(src)? {
@@ -13,4 +24,53 @@ pub fn copy_dir_all(src: impl AsRef<Path>, dst: impl AsRef<Path>) -> io::Result<
         }
     }
     Ok(())
+}
+
+
+///压缩指定路径的所有文件到zip
+///###Example:
+///```
+/// use rcmd_core::util::filesys::zip_dir;
+/// use rcmd_core::Ex::walkdir::WalkDir;
+///
+/// let zipfile = std::fs::File::create(target).unwrap();
+/// let dir  = WalkDir::new(src_dir);
+/// zip_dir(&mut dir.into_iter().filter_map(|e|e.ok()), src_dir.to_str().unwrap(), zipfile);
+///```
+///
+///
+pub fn zip_dir<T>(it: &mut dyn Iterator<Item=DirEntry>, prefix: &str, writer: T) -> zip::result::ZipResult<()>
+    where T: Write + Seek {
+    let mut zip = zip::ZipWriter::new(writer);
+    let options = FileOptions::default()
+        .compression_method(zip::CompressionMethod::Bzip2)//直接用了bzip2压缩方式，其它参看枚举
+        .unix_permissions(0o755);//unix系统权限
+
+    let mut buffer = Vec::new();
+    for entry in it {
+        let path = entry.path();
+        //zip压缩一个文件时，会把它的全路径当成文件名(在下面的解压函数中打印文件名可知)
+        //这里是去掉目录前缀
+        println!("prefix  {:?} ...", prefix);
+        let name = path.strip_prefix(Path::new(prefix)).unwrap();
+        println!("name  {:?} ...", name);
+        // Write file or directory explicitly
+        // Some unzip tools unzip files with directory paths correctly, some do not!
+        if path.is_file() {
+            println!("adding file {:?} as {:?} ...", path, name);
+            zip.start_file_from_path(name, options)?;
+            let mut f = File::open(path)?;
+
+            f.read_to_end(&mut buffer)?;
+            zip.write_all(&*buffer)?;
+            buffer.clear();
+        } else if name.as_os_str().len() != 0 {//目录
+            // Only if not root! Avoids path spec / warning
+            // and mapname conversion failed error on unzip
+            println!("adding dir {:?} as {:?} ...", path, name);
+            zip.add_directory_from_path(name, options)?;
+        }
+    }
+    zip.finish()?;
+    Result::Ok(())
 }
