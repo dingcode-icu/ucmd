e6c15078b87f306b000eeb9a261811189d007754
dwb <duanwenbin@zuoyebang.com>
2022-08-15 18:53:05 +0800

    dev 改字段

--- proj/src/subcmd/build_player.rs
+++ proj/src/subcmd/build_player.rs
@@ -2,7 +2,7 @@ use log::info;
 use rcmd_core::clap::ArgMatches;
 
 use crate::subcmd::basecmd::{BaseCmd, HookSupport};
-use std::{process::exit, path::Path};
+use std::{path::Path, process::exit};
 
 use super::BuildType;
 
@@ -10,26 +10,57 @@ struct BuildPlayer {
     ///目标平台
     platform: String,
     ///工程路径
-    proj_path: String
+    proj_path: String,
 }
 
 impl BaseCmd for BuildPlayer {
     fn run(&self) {
         info!("build-player in ->{}", self.proj_path);
-        let conf_file = Path::new(&self.proj_path).join(".ucmd"); 
+        let conf_file = Path::new(&self.proj_path).join(".ucmd");
         let build_config = BuildPlayer::parse_yaml(conf_file.to_str().unwrap());
+        let mut m_ucmdex_args;
         let ucmdex_args = build_config["ex_args"].as_str().unwrap();
-        
+
+        //chk build path
+        //append ucmndex_args
+        let build_path = std::env::current_dir()
+            .unwrap()
+            .join(".ucmd_build")
+            .join(&self.platform);
+        //output path
+        if !Path::new(build_path.as_path()).is_dir() {
+            fs::create_dir(build_path.as_path()).expect("create dir <.ucmd_build> falied!");
+        }
+        //_outputPath
+        //_targetPlatform
+        m_ucmdex_args += ucmdex_args.to_string()
+            + format!("-_outputPath:{}", build_path.display()).as_str()
+            + format!("-_targetPlatform:{}", &self.platform).as_str();
+
         // before hook
-        let bf_p = vec![ucmdex_args.to_string()];
-        self.execute_hook(self.proj_path.as_str(), HookSupport::BeforeBinBuild, &bf_p);
+        let hook_params = vec![m_ucmdex_args.to_string()];
+        self.execute_hook(
+            self.proj_path.as_str(),
+            HookSupport::BeforeBinBuild,
+            &hook_params,
+        );
         // bin execute
-        let suc = self.gen_target(self.proj_path.as_str(), &build_config, &self.platform,  if self.platform == "ios" { BuildType::Ios } else {BuildType::Android}, "");
+        let suc = self.gen_target(
+            self.proj_path.as_str(),
+            &build_config,
+            &self.platform,
+            build_path.display(),
+            m_ucmdex_args,
+        );
         if !suc {
             exit(2);
         }
         // after hook
-        self.execute_hook(self.proj_path.as_str(), HookSupport::AfterBinBuild, &bf_p);
+        self.execute_hook(
+            self.proj_path.as_str(),
+            HookSupport::AfterBinBuild,
+            &hook_params,
+        );
     }
 }
 
@@ -37,32 +68,38 @@ impl BuildPlayer {
     fn new(path: &str, platform: String) -> Self {
         BuildPlayer {
             platform,
-            proj_path:path.to_string()
+            proj_path: path.to_string(),
         }
     }
 }
 
 pub fn handle(subm: &ArgMatches) {
     let target = subm.value_of("platform");
-    match target{
+    match target {
         None => {}
         Some(_) => {}
     }
 
     let cur_dir = std::env::current_dir().unwrap();
     let cur_path = cur_dir.to_str().unwrap();
-    let proj_path = subm.value_of("path").unwrap_or_else(||cur_path);     //这里其实也不用match了 clap的require参数不符合clap就会过滤掉
+    let proj_path = subm.value_of("path").unwrap_or_else(|| cur_path); //这里其实也不用match了 clap的require参数不符合clap就会过滤掉
     let cmd = &BuildPlayer::new(proj_path, target.unwrap().to_string());
     cmd.run();
 }
 
 #[test]
 fn test_buildplayer() {
-    let proj_path = std::env::current_dir().unwrap()
-                    .parent().unwrap()
-                    .join("test");
+    let proj_path = std::env::current_dir()
+        .unwrap()
+        .parent()
+        .unwrap()
+        .join("test");
     let cmd = &BuildPlayer::new(proj_path.to_str().unwrap(), "ios".to_string());
     // cmd.run();
-    
-    cmd.execute_hook(proj_path.to_str().unwrap() , HookSupport::BeforeBinBuild, &vec!["a".to_string(), "b".to_string()])
+
+    cmd.execute_hook(
+        proj_path.to_str().unwrap(),
+        HookSupport::BeforeBinBuild,
+        &vec!["a".to_string(), "b".to_string()],
+    )
 }
--- proj/src/subcmd/players/unity.rs
+++ proj/src/subcmd/players/unity.rs
@@ -3,25 +3,23 @@ use std::path::Path;
 use log::info;
 use rcmd_core::chrono::Local;
 use rcmd_core::yaml_rust::Yaml;
-
-use crate::subcmd::BuildType;
 use super::BinCmd;
 
 pub struct UnityProj<'a> {
     proj_path:&'a str,
     config: &'a Yaml, 
     plat: &'a str, 
-    build_type: BuildType, 
+    build_path: &'a str, 
     ex_cmd: &'a str
 }
 
 impl<'a> UnityProj<'a> {
-    pub fn new(proj_path:&'a str, config: &'a Yaml, plat: &'a str, build_type: BuildType, ex_cmd: &'a str) ->Self{
+    pub fn new(proj_path:&'a str, config: &'a Yaml, plat: &'a str, build_path:&'a str, ex_cmd: &'a str) ->Self{
         UnityProj{
             proj_path,
             config,
             plat,
-            build_type,
+            build_path,
             ex_cmd,
         }
     }
@@ -34,28 +32,19 @@ impl UnityProj<'_>{
         let unity_proj = self.proj_path;
         let method = config["method"].as_str().unwrap();
         let ucmdex_args = config["ex_args"].as_str().unwrap();
-        let platform_arg =  format!(" -_targetPlatform:{}", &self.plat);
         //log file
         let log_path = std::env::current_dir().unwrap().join(".ucmd_build");
         let log_f = log_path.join(format!("{}_unity.log",  Local::now().format("%Y_%m%d_%H%M")));
-        //output path 
-        let output_path = log_path.join(self.plat);
-        if !Path::new(log_path.as_path()).is_dir(){
-            fs::create_dir(log_path.as_path()).expect("create dir <.ucmd_build> falied!");
-        }
-
         let args_str = format!("{args_base} \
         -executeMethod {method} \
         -projectPath {unity_proj} \
         -logFile {log_file} \
-        -outputPath:{output_path} \
         {ex_cmd}",
                                 args_base = args_base,
-                                output_path = output_path.display(),
                                 method = method,
-                                log_file = log_f.display().to_string(),
                                 unity_proj = unity_proj,
-                                ex_cmd = ucmdex_args.to_string() + &platform_arg
+                                log_file = log_f.display().to_string(),
+                                ex_cmd = ucmdex_args.to_string()
         );
         
         let args:Vec<String> = args_str.split(" ").map(|v|v.to_string()).collect();
